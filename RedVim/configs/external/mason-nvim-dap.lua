f8849020afb3bf8bfee82cc480807401fc8855a3
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-09-03 06:10:55 +0530

    feat: zshrc, alacritty,neofetch

---
+++ RedVim/configs/external/mason-nvim-dap.lua
@@ -0,0 +1,32 @@
+require("mason-nvim-dap").setup {
+  ensure_installed = { "python", "js", "codelldb" },
+  handlers = {
+    function(config)
+      require("mason-nvim-dap").default_setup(config)
+    end,
+    python = function(config)
+      require("mason-nvim-dap").default_setup(config) -- don't forget this!
+    end,
+    codelldb = function(config)
+      require("mason-nvim-dap").default_setup(config)
+    end,
+    dart = function(config)
+      config.adapters = {
+        type = "executable",
+        command = "dart",
+        args = { "debug_adapter" },
+      }
+      config.configurations = {
+        {
+          type = "dart",
+          request = "launch",
+          name = "Launch Dart Program",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+          args = { "--help" }, -- Note for Dart apps this is args, for Flutter apps toolArgs
+        },
+      }
+      require("mason-nvim-dap").default_setup(config) -- don't forget this!
+    end,
+  },
+}
