7c8b34629dd7f02f651ff6d17481adbefc4e87b5
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2024-01-15 01:01:02 +0530

    feat: add which key for global keymaps

--- lua/falconcodes/plugins/which-key.lua
+++ lua/falconcodes/plugins/which-key.lua
@@ -76,11 +76,18 @@ return {
 				n = { "<cmd>Lspsaga diagnostic_jump_next<cr>", "Go To Next Diagnostic" },
 				N = { "<cmd>Lspsaga diagnostic_jump_prev<cr>", "Go To Previous Diagnostic" },
 			},
+			-- Terminal
+			-- t = {
+			--     name = "Terminal",
+			--     n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
+			--     u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
+			--     t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
+			--     p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
+			--     f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
+			--     h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
+			--     v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+			-- },
 
-			-- Tagbar for function list
-			T = {
-				f = { ":TagbarToggle<cr>", "Functions List Tagbar" },
-			},
 			-- Code Formatting
 			F = {
 				name = "Code Formoat",
