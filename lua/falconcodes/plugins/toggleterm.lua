e10f72746c815bd588aad01bf6cd1bff75297623
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2024-01-15 04:00:09 +0530

    chore: reverting to old toggleterm setup func

--- lua/falconcodes/plugins/toggleterm.lua
+++ lua/falconcodes/plugins/toggleterm.lua
@@ -3,19 +3,20 @@ local config = function()
 
 	toggleterm.setup({
 		size = 13,
-		open_mapping = [[<C-\>]],
+		open_mapping = [[<c-\>]],
 		shade_filetypes = {},
 		shade_terminal = true,
 		shading_factor = 2,
+		start_in_insert = true,
 		persist_size = true,
 		direction = "float",
 		autochdir = true,
-		hidden = true,
 	})
 end
 
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
+	lazy = false,
 	config = config,
 }
