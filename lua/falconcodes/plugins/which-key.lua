67296b130082904b9f30da5425a754ea2f876a2f
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2024-01-15 02:08:44 +0530

    fix: added alternate remaps to open toggleterm

--- lua/falconcodes/plugins/which-key.lua
+++ lua/falconcodes/plugins/which-key.lua
@@ -77,16 +77,16 @@ return {
 				N = { "<cmd>Lspsaga diagnostic_jump_prev<cr>", "Go To Previous Diagnostic" },
 			},
 			-- Terminal
-			-- t = {
-			--     name = "Terminal",
-			--     n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
-			--     u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
-			--     t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
-			--     p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
-			--     f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-			--     h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-			--     v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
-			-- },
+			t = {
+				name = "Terminal",
+				-- n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
+				-- u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
+				-- t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
+				-- p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
+				f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
+				h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
+				v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+			},
 
 			-- Code Formatting
 			F = {
