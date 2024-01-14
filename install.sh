e56b88f7cb309afb4bf476a8160f40a14bf10ef0
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2024-01-15 04:49:48 +0530

    creating a simple install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dotfiles_dir="$HOME/keep-coding/falcon-dots"
+
+# List of dotfiles to symlink
+dotfiles=("zshrc" "alacritty" "kitty" "nvim" "configs")
+
+for file in "${dotfiles[@]}"; do
+    source_path="$dotfiles_dir/$file"
+    target_path="$HOME/.$file"
+
+    # Backup existing dotfiles
+    if [ -e "$target_path" ] || [ -h "$target_path" ]; then
+        mv "$target_path" "$target_path.bak"
+        echo "Backed up: .$file"
+    fi
+
+    # Create symlinks
+    ln -s "$source_path" "$target_path"
+    echo "Symlinked: .$file"
+done
+
+echo "Dotfiles installation complete."
+
