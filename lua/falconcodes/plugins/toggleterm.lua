3b91baf099682ccb32a2633d4ea0563c947cefdd
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2024-01-15 02:07:37 +0530

    fix: tried to fix toggleterm not opening bug

--- lua/falconcodes/plugins/toggleterm.lua
+++ lua/falconcodes/plugins/toggleterm.lua
@@ -1,20 +1,21 @@
 local config = function()
-    local toggleterm = require("toggleterm")
+	local toggleterm = require("toggleterm")
 
-    toggleterm.setup({
-        size = 13,
-        open_mapping = [[<C-\>]],
-        shade_filetypes = {},
-        shade_terminal = true,
-        shading_factor = 2,
-        persist_size = true,
-        direction = "float",
-        autochdir = true,
-    })
+	toggleterm.setup({
+		size = 13,
+		open_mapping = [[<C-\>]],
+		shade_filetypes = {},
+		shade_terminal = true,
+		shading_factor = 2,
+		persist_size = true,
+		direction = "float",
+		autochdir = true,
+		hidden = true,
+	})
 end
 
 return {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = config,
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = config,
 }
