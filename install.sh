68256b621853bcaa0c90fe1a9fc4419fd267a263
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2024-01-15 05:54:29 +0530

    feat: added kitty config

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-DOT_FOLDERS="alacritty,zsh,nvim,tmux,nvim-old,nvchad"
+DOT_FOLDERS="alacritty,zsh,nvim,tmux,nvim-old,nvchad,kitty"
 
 for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
     echo "[+] Folder :: $folder"
